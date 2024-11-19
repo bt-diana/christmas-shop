fd775feae8c5820307e0d659b8ed550f7accd58e
Diana Butiakova <butyakova01@gmail.com>
2024-11-20 02:30:48 +0600

    feat(home): add hero section

--- src/scripts/global.js
+++ src/scripts/global.js
@@ -1,4 +1,6 @@
+import '../styles/normilize.sass';
 import '../styles/global.sass';
 import '../styles/typography.sass';
 import '../styles/navigation-bar.sass';
 import '../styles/tabs.sass';
+import '../styles/main.sass';
--- src/scripts/home.js
+++ src/scripts/home.js
@@ -1,3 +1,4 @@
 import './global.js';
 import '../styles/home.sass';
+import '../styles/hero-section.sass';
 console.log('test home');
