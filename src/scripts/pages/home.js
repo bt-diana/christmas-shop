7ca7e65ae55f938991967db2d090bae7e91039bc
Diana Butiakova <butyakova01@gmail.com>
2024-11-30 02:36:53 +0600

    feat(home): add 4 random best gifts cards generation

--- src/scripts/pages/home.js
+++ src/scripts/pages/home.js
@@ -1,2 +1,17 @@
 import '../global.js';
 import '../../styles/pages/home.sass';
+import createGiftCard from '../../scripts/components/giftCard.js';
+import * as data from '../../assets/data/gifts.json';
+
+const gifts = data.default;
+const bestGiftsAmount = 4;
+
+//Best Gifts Generation
+const bestGiftsContainer = document.querySelector('.best-gifts');
+const selectedGifts =  gifts.sort(() => 0.5 - Math.random()).slice(0, bestGiftsAmount);
+
+selectedGifts.forEach((gift) => {
+    const giftCard = createGiftCard(gift.category, gift.name, gift.description, gift.superpowers);
+    giftCard.classList.add('best-gifts__card');
+    bestGiftsContainer.appendChild(giftCard);
+})
