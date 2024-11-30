6a360abd1750bd2ec7a869238130033c2db56aac
Diana Butiakova <butyakova01@gmail.com>
2024-12-01 01:38:25 +0600

    feat: disable scroll on menu open

--- src/scripts/layout/header.js
+++ src/scripts/layout/header.js
@@ -4,6 +4,17 @@ export default function bindOnNavigationButtonClick() {
         e.currentTarget.classList.toggle('close-button');
         e.currentTarget.classList.toggle('burger-button');
         const navigation = document.querySelector('.navigation')
-        if (navigation) navigation.classList.toggle('navigation--open');
+        if (navigation) {
+            navigation.classList.toggle('navigation--open');
+            console.log(navigation.classList.contains('navigation--open'))
+            const body = document.querySelector('body')
+            if (navigation.classList.contains('navigation--open')) {
+                body.style.overflowY = 'hidden';
+                window.scrollTo(0, 0);
+            } else {
+                body.style.overflow = 'auto';
+            }
+        }
+        
     }
 }
