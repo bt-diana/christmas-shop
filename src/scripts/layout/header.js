580e161397101be4231ecf9ee8ce65c0b96f0a7d
Diana Butiakova <butyakova01@gmail.com>
2024-12-01 01:19:44 +0600

    feat: open navigation menu on burger button click and close on close nutton click

--- src/scripts/layout/header.js
+++ src/scripts/layout/header.js
@@ -3,5 +3,7 @@ export default function bindOnNavigationButtonClick() {
     navigationButton.onclick = (e) => {
         e.currentTarget.classList.toggle('close-button');
         e.currentTarget.classList.toggle('burger-button');
+        const navigation = document.querySelector('.navigation')
+        if (navigation) navigation.classList.toggle('navigation--open');
     }
 }
