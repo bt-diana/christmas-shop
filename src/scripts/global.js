6a3721129889edac057bf7f852b7af3907e5c54d
Diana Butiakova <butyakova01@gmail.com>
2024-12-02 21:01:04 +0600

    feat: add back to top button

--- src/scripts/global.js
+++ src/scripts/global.js
@@ -1,4 +1,22 @@
 import '../styles/global.sass';
 import bindOnNavigationButtonClick from '../scripts/layout/header.js';
 
+//navigation
 bindOnNavigationButtonClick();
+
+//back to top button
+const backToTopButton = document.querySelector('.back-to-top-button');
+
+backToTopButton.onclick = () => {
+    window.scrollTo(0, 0);
+}
+
+window.addEventListener("scroll", () => {
+    if (document.documentElement.scrollTop >= 300) {
+        if (!backToTopButton.classList.contains('back-to-top-button--show')) 
+            backToTopButton.classList.add('back-to-top-button--show')
+    } else {
+        if (backToTopButton.classList.contains('back-to-top-button--show')) 
+            backToTopButton.classList.remove('back-to-top-button--show')
+    }
+});
